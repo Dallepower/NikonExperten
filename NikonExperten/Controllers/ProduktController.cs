20c7775af5b17d9e9340a1cfb0e5766c16eea94e
Dallepower <danieljensen92@gmail.com>
2015-12-15 10:22:56 +0100

    produktliste bla  bla  bla

--- NikonExperten/Controllers/ProduktController.cs
+++ NikonExperten/Controllers/ProduktController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RepoNE;
 
-namespace NikonExperten.Views
+namespace NikonExperten.Controllers
 {
     public class ProduktController : Controller
     {
+        ProduktFac prodFac = new ProduktFac();
+
         // GET: Produkt
-        public ActionResult Produkter()
+        public ActionResult ProduktListe(int id=1)
         {
-            return View();
+            return View(prodFac.GetProduktListe(id));
         }
+
     }
 }
