f37ebdfe9fe0e7ede0e4c7e5d2aada1d7682e49c
Mikkel Kloth Snog Greve <mikk6569@videndjurs.net>
2015-12-15 12:47:29 +0100

    tt

--- RepoNE/Factories/ProduktFac.cs
+++ RepoNE/Factories/ProduktFac.cs
@@ -7,15 +7,15 @@ namespace RepoNE
 {
 	 public class ProduktFac:AutoFac<Produkt>
 	 {
-        KategoriFac katFac = new KategoriFac();
+      //  KategoriFac katFac = new KategoriFac();
 
-	     public ProduktListe GetProduktListe(int katID)
-	     {
-	         ProduktListe pl = new ProduktListe();
-	         pl.Kategori = katFac.Get(katID);
-	         pl.Produkter = GetBy("KatID", katID);
-	         return pl;
-	     }
+	     //public ProduktListe GetProduktListe(int katID)
+	     //{
+	     //    ProduktListe pl = new ProduktListe();
+	     //    pl.Kategori = katFac.Get(katID);
+	     //    pl.Produkter = GetBy("KatID", katID);
+	     //    return pl;
+	     //}
 
 	 }
 
