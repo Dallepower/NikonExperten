960423754d36e533142fb8f4ece2c7ce9526f8ff
Mikkel Kloth Snog Greve <mikk6569@videndjurs.net>
2015-12-15 12:26:25 +0100

    oprettet produkt filer

---
+++ NikonExperten/Areas/CMS/Controllers/aProduktController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NikonExperten.Areas.CMS.Controllers
+{
+    public class aProduktController : Controller
+    {
+        // GET: CMS/aProdukt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RepoNE/Models/BaseModels/Produkt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoNE
+{
+    public class Produkt
+    {
+        public int ID { get; set; }
+
+        public string Navn { get; set; }
+
+        public string Varenummer { get; set; }
+
+        public int Pris { get; set; }
+
+        public int Tilbudspris { get; set; }
+
+        public string Leveringstid { get; set; }
+
+        public string Lagerantal { get; set; }
+
+        public string Billede { get; set; }
+
+        public string Producent { get; set; }
+
+        public string Beskrivelse { get; set; }
+
+        public int KatID { get; set; }
+
+    }
+}
