6f57eeb5e68db7fbdcd962f32fb2976341903818
Mikkel Kloth Snog Greve <mikk6569@videndjurs.net>
2015-12-18 08:56:42 +0100

    Lavet lidt models og andet diverse

---
+++ NikonExperten/Areas/CMS/Controllers/BrugerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Helpers;
+using System.Web.Security;
+using RepoNE;
+
+namespace NikonExperten.Areas.CMS.Controllers
+{
+    public class BrugerController : Controller
+    {
+        BrugerFac bf = new BrugerFac();
+        // GET: CMS/Bruger
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string Navn, string Adgangskode)
+        {
+            Bruger bruger = bf.Login(Navn.Trim(), Crypto.Hash(Adgangskode.Trim()));
+
+            if (bruger.ID > 0)
+            {
+                FormsAuthentication.SetAuthCookie(bruger.ID.ToString(), false);
+
+                Session["UserID"] = bruger.ID;
+                Session["Level"] = bruger.Level;
+                Session.Timeout = 120;
+                return Redirect("/CMS/Bruger/Secret/");
+            }
+            else
+            {
+                ViewBag.MSG = "Brugeren blev ikke fundet!!!";
+                return View("Index");
+            }
+        }
+
+        [Authorize]
+        public ActionResult Secret()
+        {
+            return View();
+        }
+
+    }
+}
--- RepoNE/Models/BaseModels/Bruger.cs
+++ RepoNE/Models/BaseModels/Bruger.cs
@@ -1,22 +1,26 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RepoNE
 {
+    
+        public class Bruger
+        {
+            public int ID { get; set; }
 
-	 public class Bruger
-	{
-		 public int ID { get; set; }
+            public string Navn { get; set; }
 
-		 public string Navn { get; set; }
+            public string Adgangskode { get; set; }
 
-		 public string Adgangskode { get; set; }
+            public string Email { get; set; }
 
-		 public string Email { get; set; }
+            
 
-		 public string Transfer { get; set; }
+            public int Level { get; set; }
 
-		 public int Level { get; set; }
-
-	 }
-
+        }
+    
 }
